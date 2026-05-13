98e720d5f000f499e01abdd28fb98b2842a1313c
Erik? <25traned@MacBook-Mini.local>
2026-05-13 12:29:35 +1000

    Update LinkedIn link

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -6,7 +6,7 @@ export const FOOTER_LINKS: FooterLink[] = [
     name: `LinkedIn`,
     hoverText: `Connect with me`,
     icon: `icons/linkedin.svg`,
-    url: `https://www.linkedin.com/in/ethan-tran-29467b33a/`,
+    url: `https://www.linkedin.com/in/ethandvtran/`,
   },
   {
     name: `GitHub`,
